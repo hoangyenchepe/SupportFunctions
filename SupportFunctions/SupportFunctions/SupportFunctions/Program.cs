a58beb9dc338c6db48b401cc413ea69368a03757
YenVoChePe <yen.vo@tekcent.com>
2023-03-26 12:07:45 +0700

    test

---
+++ SupportFunctions/SupportFunctions/SupportFunctions/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SupportFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ExcelTools excelTools = new ExcelTools();
+            excelTools.Read();
+            Console.ReadLine();
+        }
+    }
+}
